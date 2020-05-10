dc38eea941377c6471d594348cadb60ffd9585ea
Matthew Wagaman <averaqedev@gmail.com>
2020-05-10 00:14:12 -0700

    added people service and people router

--- petful-server-template/modules/people/people.router.js
+++ petful-server-template/modules/people/people.router.js
@@ -7,10 +7,27 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   // Return all the people currently in the queue.
+  return res.json(People.get());
 });
 
 router.post('/', json, (req, res) => {
   // Add a new person to the queue.
+  const { person } = req.body;
+
+  if(!person) {
+    return res.status(400).json({
+      error: 'Invalid `name`'
+    });
+  }
+  if(typeof person !== 'string') {
+    return res.status(400).json({
+      error: '`name` must be a string'
+    });
+  }
+
+  People.enqueue(person);
+
+  return res.status(201).json({ person });
 });
 
 module.exports = router;
